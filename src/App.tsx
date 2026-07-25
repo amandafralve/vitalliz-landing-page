f6479d58ae3a08ff7cae6db9c0647080914d7722
amandafralve <amanda.fralve@gmail.com>
2026-07-25 00:12:31 -0300

    commit inicial: adiciona esqueleto navbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components/Navbar/Navbar"
+import './styles/global.css'
+import './styles/theme.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import styles from "./Navbar.module.css";
+
+export function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <img src="/vitallizLogo.svg" alt="Logo Vitaliz" />
+            <p>Início</p>
+            <p>Projeto</p>
+            <p>Sobre</p>
+            <p>Equipe</p>
+            <button>Ver Projetos</button>
+            <button>PT_BR</button>
+        </nav>
+    );
+}
