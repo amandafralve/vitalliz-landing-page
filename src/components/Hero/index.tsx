52c012408554ebc6fa49d4d216fd0cf7d0c11b43
amandafralve <amanda.fralve@gmail.com>
2026-08-05 14:20:42 -0300

    feat(hero): adicionado background animado reactbits no hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,11 +1,36 @@
 import { MoveRight } from "lucide-react";
 import { Button } from "../Button";
 import { Container } from "../Container";
+import GradientWaves from "../GradientWaves"
 import styles from "./styles.module.css"
 
 export function Hero() {
     return (
         <section className={styles.hero}>
+            <GradientWaves
+                className={styles.heroBackground}
+                horizonColor="#1585C4"
+                waveColor="#1585C4"
+                crestColor="#31803A"
+                speed={0.4}
+                amplitude={2.5}
+                waveScale={0.6}
+                waveRatio={0.9}
+                swell={35}
+                turbulence={20}
+                tilt={0.8}
+                zoom={1}
+                height={3}
+                fogDepth={20}
+                detail="medium"
+                brightness={1}
+                opacity={1}
+                mouseInteraction
+                parallaxStrength={0.5}
+                grain
+                grainIntensity={0.05}
+            />
+
             <Container>
                 <div className={styles["hero-content"]}>
                     <div className={styles.heroTitle}>
@@ -14,7 +39,6 @@ export function Hero() {
                     </div>
                     
                     <p>Somos uma equipe de desenvolvimento mobile e web. Transformamos desafios do dia a dia em soluções inteligentes e sustentáveis - como o Nitrusleaf, nosso app de diagnóstico agrícola por inteligência artificial.</p>
-
                     <div className={styles.buttonsDiv}>
                         <Button icon={<MoveRight/>} color="white" text="Explorar Projeto" />
                         <Button color="transparent" text="Fale conosco" />
