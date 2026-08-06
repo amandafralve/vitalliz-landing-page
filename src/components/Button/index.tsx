4f2403d3b5761a26fd5dbf1a431a811dee4c8b0f
amandafralve <amanda.fralve@gmail.com>
2026-08-06 00:15:39 -0300

    style(button): animação em variação de botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,34 @@ import styles from "./styles.module.css"
 
 type ButtonProps = {
     icon?: React.ReactNode;
+    iconPosition?: "left" | "right";
     text: string;
-    color?: "blue" | "white" | "transparent" | "language";
+    color?: "blue" | "white"| "whiteAnimated" | "transparent" | "language";
+    size?: "sm" | "md" | "lg";
 } & React.ComponentProps<'button'>
 
-export function Button({ icon, text, color="blue", ...props}: ButtonProps) {
+export function Button({ 
+    icon,
+    iconPosition = "left", 
+    text, 
+    color="blue",
+    size = "md",
+    ...props}: ButtonProps
+) {
     return (
-        <button className={`${styles.button} ${styles[color]}`} {...props}>
-            {icon}{text}
+        <button
+            className={`${styles.button} ${styles[color]} ${styles[size]}`}
+            {...props}
+        >
+            {icon && iconPosition === "left" && (
+                <span className={styles.iconWrapper}>{icon}</span>
+            )}
+
+            <span className={styles.textWrapper}>{text}</span>
+
+            {icon && iconPosition === "right" && (
+                <span className={styles.iconWrapper}>{icon}</span>
+            )}
         </button>
     );
 }
