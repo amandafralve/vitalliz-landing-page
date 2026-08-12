b71e154d6667424134d970bd2e295edf61b9440f
amandafralve <amanda.fralve@gmail.com>
2026-08-12 14:13:50 -0300

    style: seção about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,11 +1,22 @@
 import styles from './styles.module.css'
 import { Container } from '../Container';
+import { CarouselWithCaptions } from '../Carousel';
 
 export function About() {
     return (
         <Container>
-            <div className={styles.aboutUs}>
-                <h1>Sobre nós</h1>
+            <div className={styles.containerAbout}>
+                <div className={styles.aboutUs}>
+                    <h1>Sobre nós</h1>
+                    <p>
+                        A Vitalliz é a equipe criada para o desenvolvimento do Projeto
+                        Integrador do curso de Desenvolvimento de Software Multiplataforma (Fatec), com o propósito de unir tecnologia, inovação e sustentabilidade - alinhado ao <strong>ODS 2 da ONU</strong> (Fome Zero e Agricultura Sustentável)
+                    </p>
+                    <div>
+                    </div>
+                </div>
+                <CarouselWithCaptions />
+                
             </div>
         </Container>
     );
